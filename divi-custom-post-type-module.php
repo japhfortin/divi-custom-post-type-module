8ffb1a4ba683f0ffb5fa9023d4bbc7008bd0d491
japhfortin <japhfortin@gmail.com>
2017-07-03 14:44:59 +0800

    Add files via upload

---
+++ divi-custom-post-type-module.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: Divi Custom Post Type Module
+* Plugin URI: https://www.elegantthemes.com/
+* Description: Display content of any custom post type just like the blog module
+* Version: 1.0
+* Author: Japh Fortin
+* Author URI: http://japhfortin.com/
+**/
+
+include 'includes/custom-post-type-module.php';
